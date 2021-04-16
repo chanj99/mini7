6195f22f7af8d71bbc3399c5900d948e3842338a
user1 <jenny991119@naver.com>
2021-04-16 22:06:02 +0900

    add manager.h

--- manager.h
+++ manager.h
@@ -1,1 +1,7 @@
+#include "product.h"
+
+void listProduct( Product *p, int count);//목록 전체 읽기
+void search(Product *p, int count); //검색 하기 기능
+void saveData(Product *p, int count); //파일 저장 기능
+int loadData(Product *p); //파일 불러오기 기능
 
